4fd53361c36c7363494cd2b95626acf758d7e8d4
inesbarradas <inesmartinsbarradas1996@gmail.com>
2019-03-29 11:03:00 +0000

    resolução do exercicio

--- MyFirstMethod/Program.cs
+++ MyFirstMethod/Program.cs
@@ -4,19 +4,28 @@ namespace MyFirstMethod
 {
     class Program
     {
+        static void CountToN(int n)
+        {
+            for(int i = 1; i <= n; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
         static void CountTo10()
         {
-            for(int i = 1; i <= 10; i++)
-{
+            for (int i = 1; i <= 10; i++)
+            {
                 Console.WriteLine(i);
             }
         }
 
         static void Main(string[] args)
         {
+            CountToN(3);
+            CountToN(9);
             CountTo10();
             CountTo10();
-
 
         }
     }
